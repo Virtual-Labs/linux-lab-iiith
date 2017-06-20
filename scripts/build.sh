e1e35565b53ca2262f6fcb87a1d43af5285b027a
ayogi <yogeshiiith@gmail.com>
2017-06-20 23:37:40 +0530

    write function to update ldapexec.php file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ldap_config=/etc/ldapscripts/ldapscripts.conf
+ldap_exec=/var/www/html/php/ldapexec.php
 
 function install_packages()
 {
@@ -31,6 +32,31 @@ function update_ldapscripts()
  sed -i 's/MIDSTART=.*/MIDSTART="10000"/g' "$ldap_config"
 }
 
+function create_password_file()
+{
+ sudo sh -c "echo -n 'password' > /etc/ldapscripts/ldapscripts.passwd"
+ sudo chmod 440 /etc/ldapscripts/ldapscripts.passwd
+}
+
+function update_ldapexec_file()
+{
+ echo "enter ldap ip: "
+ read ldap_ip
+ sed '0,/$ldap_host =.*/s//$ldap_host = "$ldap_ip"/' $ldap_exec
+ echo "enter ldap password: "
+ read -s ldap_password
+ echo "confirm password: "
+ read -s ldap_confirm_password
+
+ if [ $ldap_password != $ldap_confirm_password ]
+ then
+    echo "password does not match"
+ else
+    sed '0,/$ldap_admin_pass =.*/s//$ldap_admin_pass = "$ldap_password"/' $ldap_exec
+ fi
+}
+
 install_packages
 build_lab
 update_ldapscripts
+create_password_file
