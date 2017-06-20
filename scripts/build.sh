1c973fec4cdb7dfe837cdabfa3e1384cc81ed1f4
ayogi <yogeshiiith@gmail.com>
2017-06-20 23:07:52 +0530

    write function to update ldapscripts configuration

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ldap_config=/etc/ldapscripts/ldapscripts.conf
+
 function install_packages()
 {
  sudo apt-get install php5 -y
@@ -18,5 +20,17 @@ function build_lab()
  sudo rsync -ar ~/linux-lab-iiith/build/ /var/www
 }
 
+function update_ldapscripts()
+{
+ sed -i "s/#SERVER=.*/SERVER=ldap:\/\/localhost/g" "$ldap_config"
+ sed -i "s/#SUFFIX=.*/SUFFIX='dc=virtual-labs,dc=ac,dc=in'/g" "$ldap_config"
+ sed -i "s/#GSUFFIX=.*/GSUFFIX='ou=Group'/g" "$ldap_config"
+ sed -i "s/#USUFFIX=.*/USUFFIX='ou=People'/g" "$ldap_config"
+ sed -i "s/#MSUFFIX=.*/MSUFFIX='ou=Computers'/g" "$ldap_config"
+ sed -i "s/BINDDN=.*/BINDDN='cn=admin,dc=virtual-labs,dc=ac,dc=in'/g" "$ldap_config"
+ sed -i 's/MIDSTART=.*/MIDSTART="10000"/g' "$ldap_config"
+}
+
 install_packages
 build_lab
+update_ldapscripts
