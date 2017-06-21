a3b7ea235073ae43cc1bc84f323da8a70c911a6a
nikhil3456 <nikhilbansal3456@gmail.com>
2017-06-21 07:38:26 -0400

    separate out intialization part

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,45 +9,6 @@ port=8000
 content_html=/var/www/html/content.html
 frame_html=/var/www/html/exp4/interaction-frame.html
 
-######## Function for setting proxy
-function set_proxy()
-{
-###### Adding proxy in ~/.bashrc file
-
-  LINE='export http_proxy="http://proxy.iiit.ac.in:8080"'
-  FILE=~/.bashrc
-  grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE" 
-  LINE='export https_proxy="http://proxy.iiit.ac.in:8080"'
-  FILE=~/.bashrc
-  grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-  
-  export http_proxy="http://proxy.iiit.ac.in:8080"
-  export https_proxy="http://proxy.iiit.ac.in:8080"
-
-####### Adding proxy in /etc/apt/apt.conf
-
-  touch /etc/apt/apt.conf
-  LINE='Acquire::http::Proxy "http://proxy.iiit.ac.in:8080";'
-  FILE=/etc/apt/apt.conf
-  grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-  
-  LINE='Acquire::https::Proxy "http://proxy.iiit.ac.in:8080";'
-  FILE=/etc/apt/apt.conf
-  grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
- 
-}
-function install_packages()
-{
- sudo apt-get update -y
- sudo apt-get install git -y
- sudo apt-get install php5 -y
-# sudo apt-get install libgd2-noxpm -y
-# sudo apt-get install libgd2-xpm -y
-# sudo apt-get install libgvc5 -y
- sudo apt-get install graphviz -y
- sudo apt-get install ldapscripts -y
- sudo apt-get install php5-ldap
-}
 
 function build_lab()
 {
@@ -255,8 +216,6 @@ function final_setup()
 
 #######################################
 
-set_proxy
-install_packages
 #build_lab
 update_ldapscripts
 create_password_file
