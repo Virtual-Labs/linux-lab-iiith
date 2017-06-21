c55fe6e18a981fb78cb2f5080002be49bd508d18
nikhil3456 <nikhilbansal3456@gmail.com>
2017-06-21 07:39:55 -0400

    adding initializing script

---
+++ scripts/initialise.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+######## Function for setting proxy
+function set_proxy()
+{
+###### Adding proxy in ~/.bashrc file
+
+  LINE='export http_proxy="http://proxy.iiit.ac.in:8080"'
+  FILE=~/.bashrc
+  grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE" 
+  LINE='export https_proxy="http://proxy.iiit.ac.in:8080"'
+  FILE=~/.bashrc
+  grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+  
+  export http_proxy="http://proxy.iiit.ac.in:8080"
+  export https_proxy="http://proxy.iiit.ac.in:8080"
+
+####### Adding proxy in /etc/apt/apt.conf
+
+  touch /etc/apt/apt.conf
+  LINE='Acquire::http::Proxy "http://proxy.iiit.ac.in:8080";'
+  FILE=/etc/apt/apt.conf
+  grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+  
+  LINE='Acquire::https::Proxy "http://proxy.iiit.ac.in:8080";'
+  FILE=/etc/apt/apt.conf
+  grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+ 
+}
+
+function install_packages()
+{
+ sudo apt-get update -y
+ sudo apt-get install git -y
+ sudo apt-get install php5 -y
+ sudo apt-get install graphviz -y
+ sudo apt-get install ldapscripts -y
+ sudo apt-get install php5-ldap
+}
+
+set_proxy
+install_packages
+
